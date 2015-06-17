9e65ebdbf1d32ad49f805faa7be881e719b5778c
joshuamataaraya <joshuamataa@gmail.com>
2015-06-16 23:44:51 -0600

    File Creation

---
+++ FileCreator/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int i=200000;
+    float grado;
+    int id;
+    char *accion[2]={"avanzar","disparar"};
+    float valor;
+    while(i>=0)
+    {
+        srand(rand());
+
+        grado=((float)rand()/(float)RAND_MAX);
+        id=rand();
+        id= id % 1000;
+        if(id<100)
+        {
+            id=id+100;
+        }
+        valor=((float)rand()/(float)RAND_MAX)+100;
+        valor=fmod(valor,4.2);
+        if (valor <0.8)
+        {
+            valor=valor+0.8;
+        }
+
+        printf("%0.2f|%d|%s|%0.1f\n",grado,id,accion[rand()%2],valor);
+        i--;
+    }
+    return 0;
+}
