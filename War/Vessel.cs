4ddacd7b2b3dd7d4088866314e2ea1b4c663a55c
Joshua Mata <joshuamataa@gmail.com>
2015-06-16 20:32:50 -0600

    Vessel Class creation

---
+++ War/Vessel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Collections;
+namespace War
+{
+    class Vessel
+    {
+        private Point _Position;
+        public ArrayList _Instructions;
+        public Vessel()
+        {
+            _Position = new Point();
+            _Instructions = new ArrayList();
+        }
+        public Point getPostition()
+        {
+            return _Position;
+        }
+
+        public void setPosition(int pX, int pY)
+        {
+
+        }
+        public void setIntructions(ArrayList pInstructions){
+            _Instructions=pInstructions;
+        }
+        private void shoot(int pValor, int pGrado)
+        {
+
+        }
+        public void move(int pValor,int pGrado)
+        {
+
+        }
+    }
+}
